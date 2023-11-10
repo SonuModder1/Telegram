d418493921d99fd70cc8fe04edee2e7c2ec94d2b
ddos <109574396+SonuModder1@users.noreply.github.com>
2023-11-10 14:06:57 +0530

    Add files via upload

--- tviews.py
+++ tviews.py
@@ -1,17 +1,20 @@
-import aiohttp, asyncio
-from re import search
+import os
+import aiohttp
+import asyncio
+from re import compile, search
 from aiohttp_socks import ProxyConnector
 from argparse import ArgumentParser
-from re import compile
 from os import system, name
 from threading import Thread
 from time import sleep
 
+os.system('pip install aiohttp==3.8.4')
+os.system('pip install aiohttp-socks==0.8.0')
+os.system('clear')
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
-    
 REGEX = compile(
-    r"(?:^|\D)?(("+ r"(?:[1-9]|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
+    r"(?:^|\D)?((" + r"(?:[1-9]|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
     + r"\." + r"(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
     + r"\." + r"(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
     + r"\." + r"(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])"
@@ -20,33 +23,24 @@ REGEX = compile(
     + r")(?:\D|$)"
 )
 
-
 class Telegram:
-    def __init__(self, channel: str, post: int) -> None:
-        # Async Tasks
-        self.tasks = 225 
-        
+    def __init__(self, channel: str, post: int, tasks: int = 225) -> None:
+        self.tasks = tasks
         self.channel = channel
         self.post = post
-        
         self.cookie_error = 0
         self.sucsess_sent = 0
         self.failled_sent = 0
-        self.token_error  = 0
-        self.proxy_error  = 0
-
+        self.token_error = 0
+        self.proxy_error = 0
 
     async def request(self, proxy: str, proxy_type: str):
-        if proxy_type == 'socks4': connector = ProxyConnector.from_url(f'socks4://{proxy}')
-        elif proxy_type == 'socks5': connector = ProxyConnector.from_url(f'socks5://{proxy}')
-        elif proxy_type == 'https': connector = ProxyConnector.from_url(f'https://{proxy}')
-        else: connector = ProxyConnector.from_url(f'http://{proxy}')
-        
+        connector = ProxyConnector.from_url(f'{proxy_type}://{proxy}')
         jar = aiohttp.CookieJar(unsafe=True)
         async with aiohttp.ClientSession(cookie_jar=jar, connector=connector) as session:
             try:
                 async with session.get(
-                    f'https://t.me/{self.channel}/{self.post}?embed=1&mode=tme', 
+                    f'https://t.me/{self.channel}/{self.post}?embed=1&mode=tme',
                     headers={
                         'referer': f'https://t.me/{self.channel}/{self.post}',
                         'user-agent': user_agent
@@ -56,60 +50,58 @@ class Telegram:
                         views_token = search('data-view="([^"]+)"', await embed_response.text())
                         if views_token:
                             views_response = await session.post(
-                                'https://t.me/v/?views=' + views_token.group(1), 
+                                f'https://t.me/v/?views={views_token.group(1)}',
                                 headers={
                                     'referer': f'https://t.me/{self.channel}/{self.post}?embed=1&mode=tme',
                                     'user-agent': user_agent, 'x-requested-with': 'XMLHttpRequest'
                                 }, timeout=aiohttp.ClientTimeout(total=5)
                             )
-                            if (
-                                await views_response.text() == "true" 
-                                and views_response.status == 200
-                            ): self.sucsess_sent += 1
-                            else: self.failled_sent += 1
-                        else: self.token_error += 1
-                    else: self.cookie_error += 1
-            except: self.proxy_error += 1
-            finally: jar.clear()
+                            if await views_response.text() == "true" and views_response.status == 200:
+                                self.sucsess_sent += 1
+                            else:
+                                self.failled_sent += 1
+                        else:
+                            self.token_error += 1
+                    else:
+                        self.cookie_error += 1
+            except Exception as e:
+                print(f'Error in request: {e}')
+                self.proxy_error += 1
+            finally:
+                jar.clear()
 
-
-    def run_proxies_tasks(self, lines: list, proxy_type):
+    def run_proxies_tasks(self, lines: list, proxy_type: str):
         async def inner(proxies: list):
-            await asyncio.wait(
-                [asyncio.create_task(self.request(proxy, proxy_type)) 
-                for proxy in proxies])
-        chunks = [lines[i:i+self.tasks] for i in range(0, len(lines), self.tasks)]
-        for chunk in chunks: asyncio.run(inner(chunk))
-    
-    
-    def run_auto_tasks(self):
+            await asyncio.gather(*[self.request(proxy, proxy_type) for proxy in proxies])
+
+        chunks = [lines[i:i + self.tasks] for i in range(0, len(lines), self.tasks)]
+        for chunk in chunks:
+            asyncio.run(inner(chunk))
+
+    def run_auto_tasks(self, auto_proxies: list):
         while True:
-            async def inner(proxies: tuple):
-                await asyncio.wait(
-                    [asyncio.create_task(self.request(proxy, proxy_type)) 
-                    for proxy_type, proxy in proxies])
-            auto = Auto()
-            chunks = [auto.proxies[i:i+self.tasks] for i in range(0, len(auto.proxies), self.tasks)]
-            for chunk in chunks: asyncio.run(inner(chunk))
+            async def inner(proxies: list):
+                await asyncio.gather(*[self.request(proxy, proxy_type) for proxy_type, proxy in proxies])
 
+            chunks = [auto_proxies[i:i + self.tasks] for i in range(0, len(auto_proxies), self.tasks)]
+            for chunk in chunks:
+                asyncio.run(inner(chunk))
 
-    async def run_rotated_task(self, proxy, proxy_type):
-        while True: 
-            await asyncio.wait(
-                [asyncio.create_task(self.request(proxy, proxy_type)) 
-                for _ in range(self.tasks)])
-
+    async def run_rotated_task(self, proxy: str, proxy_type: str):
+        while True:
+            await asyncio.gather(*[self.request(proxy, proxy_type) for _ in range(self.tasks)])
 
     def cli(self):
         logo = '''
         ~ Telegram Auto Views V4 ~
-          ~ github.com/DarkBotnetGT ~
-               ~ @DarkBotnetGT ~
+          ~ github.com/TeaByte ~
+               ~ @TeaByte ~
         '''
         while not self.sucsess_sent:
             print(logo)
             print('\n\n        [ Waiting... ]\r')
-            sleep(0.3);system('cls' if name=='nt' else 'clear')
+            sleep(0.3)
+            system('cls' if name == 'nt' else 'clear')
 
         while True:
             print(logo)
@@ -124,37 +116,27 @@ class Telegram:
         Token Error:  {self.token_error}
         Cookie Error: {self.cookie_error}
             ''')
-            sleep(0.3);system('cls' if name=='nt' else 'clear')
+            sleep(0.3)
+            system('cls' if name == 'nt' else 'clear')
 
 
 class Auto:
-    def __init__(self):
+    def __init__(self, http_sources_file: str = 'auto/http.txt', socks4_sources_file: str = 'auto/socks4.txt',
+                 socks5_sources_file: str = 'auto/socks5.txt'):
         self.proxies = []
-        try: 
-            with open(f'auto/http.txt', 'r') as file:
-                self.http_sources = file.read().splitlines()
-                
-            with open(f'auto/socks4.txt', 'r') as file:
-                self.socks4_sources = file.read().splitlines()
-                
-            with open(f'auto/http.txt', 'r') as file:
-                self.socks5_sources = file.read().splitlines()
-                
-        except FileNotFoundError: 
-            print(' [ Error ] auto file not found!')
-            exit()
-        
-        print(' [ WAIT ] Scraping proxies... ')
+        self.http_sources_file = http_sources_file
+        self.socks4_sources_file = socks4_sources_file
+        self.socks5_sources_file = socks5_sources_file
+        self.load_sources()
         asyncio.run(self.init())
 
-
-    async def scrap(self, source_url, proxy_type):
+    async def scrap(self, source_url: str, proxy_type: str):
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(
-                    source_url, 
-                    headers={'user-agent': user_agent}, 
-                    timeout=aiohttp.ClientTimeout(total=15)
+                        source_url,
+                        headers={'user-agent': user_agent},
+                        timeout=aiohttp.ClientTimeout(total=15)
                 ) as response:
                     html = await response.text()
                     if tuple(REGEX.finditer(html)):
@@ -202,3 +184,5 @@ elif args.mode[0] == "r":
     asyncio.run(api.run_rotated_task(args.proxy, args.type))
     
 else: api.run_auto_tasks()
+
+                   
